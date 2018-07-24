cc54f1e2af8fd8f3dc05059e7aaae6dd72785290
JpSaenz <ju>
2018-07-24 15:58:19 -0500

    Solucionado problemas de merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { SummaryComponent } from './summary/summary.component';
-=======
+
 import { BasicinfoComponent } from './basicinfo/basicinfo.component';
 import { AppRoutingModule } from './/app-routing.module';
 
 import { FormsModule } from '@angular/forms';
->>>>>>> InformacionBasica
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    SummaryComponent
-=======
+    SummaryComponent,
     BasicinfoComponent
->>>>>>> InformacionBasica
   ],
   imports: [
     BrowserModule,
