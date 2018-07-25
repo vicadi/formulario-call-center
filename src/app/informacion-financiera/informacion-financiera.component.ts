c5c24ff6264b9456d793d4501967d4bda70f53ed
Xenia Viviana Cadena Diaz <xcadena@MacBook-Pro-de-adseglocdom.local>
2018-07-25 16:21:29 -0500

    informacion financiera con validaciones

--- src/app/informacion-financiera/informacion-financiera.component.ts
+++ src/app/informacion-financiera/informacion-financiera.component.ts
@@ -12,25 +12,90 @@ export class InformacionFinancieraComponent implements OnInit {
   errorMessageIncome: boolean = false;
   errorMessageOutcome: boolean = false;
   errorMessageActivos: boolean = false;
-  errorMessagePasivos: boolean = false;
-  outcome: number;
+  errorMessagePasivos: boolean = false
+  ingresos: string ='';
+  egresos: string ='';
+  activos: string ='';
+  pasivos: string ='';
   income: number;
-  activos: number;
-  pasivos: number;
-  
-  ingresosString: string='';
+  outcome: number;
+  temporal: any;
+  anterior: any;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  showComponent(){
-    if(this.income!=undefined && this.income!=null) {
-      this.errorMessageIncome = true;
+  onKeyIngresos() {
+    this.ingresos=this.ingresos.replace(/[^$0-9]/g, "");
+    if(this.ingresos.length>0){
+        this.temporal = this.ingresos.split('$');
+        if (this.temporal.length>1) {
+          if(parseInt(this.temporal[1])>0){
+            this.ingresos = '$' + this.temporal[1];   
+          }else{
+            this.ingresos=''; 
+          }
+        }else{
+          if(parseInt(this.temporal[0])>0){
+            this.ingresos = '$' + this.temporal[0];   
+          }else{
+            this.ingresos=''; 
+          }
+        }
     }
+  }
+
+  onKeyEgresos() {
+    this.egresos=this.egresos.replace(/[^$0-9]/g, "");
+    if(this.egresos.length>0){
+      this.temporal = this.egresos.split('$');
+      if (this.temporal.length>1) {
+        this.egresos = '$' + this.temporal[1];   
+      }else{
+        this.egresos = '$' + this.temporal[0];
+      }
+    }
+  }
+
+  onKeyActivos() {
+    this.activos=this.activos.replace(/[^$0-9]/g, "");
+    if(this.activos.length>0){
+      this.temporal = this.activos.split('$');
+      if (this.temporal.length>1) {
+        this.activos = '$' + this.temporal[1];   
+      }else{
+        this.activos = '$' + this.temporal[0];
+      }
+    }
+  }
+
+  onKeyPasivos() {
+    this.pasivos=this.pasivos.replace(/[^$0-9]/g, "");
+    if(this.pasivos.length>0){
+      this.temporal = this.pasivos.split('$');
+      if (this.temporal.length>1) {
+        this.pasivos = '$' + this.temporal[1];   
+      }else{
+        this.pasivos = '$' + this.temporal[0];
+      }
+    }
+  }
+
+  showComponent(){
+    this.income = parseInt(this.ingresos.split('$')[1]);
+    this.outcome = parseInt(this.egresos.split('$')[1]);
     this.showSummary = true;
     this.showFinnancial = false;
+  }
+
+  protected letOnlyNumbers(txt: string): string {
+    if (txt.match(/[0-9]/g)) {
+      return txt;
+    } else {
+      return '';
+    }
   }
 
 }
