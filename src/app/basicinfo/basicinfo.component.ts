c9a642b67a5bbc7e68ac455e717cf7ae108b65a2
JpSaenz <ju>
2018-07-25 10:03:46 -0500

    Corrección de errores pendientes de sprint pasado

--- src/app/basicinfo/basicinfo.component.ts
+++ src/app/basicinfo/basicinfo.component.ts
@@ -12,8 +12,14 @@ import { FormsModule, ControlValueAccessor } from '@angular/forms';
 
 export class BasicinfoComponent implements OnInit{
   model = new BasicInfo('','','','');
+  
+  showButton:boolean = false;
+  
   showBasicInfo:boolean = true;
   showFinancialInfo:boolean = false;
+  
+  error:boolean = false;
+  errorMessage = '';
 
   constructor() {
   }
@@ -21,19 +27,60 @@ export class BasicinfoComponent implements OnInit{
   ngOnInit() {
   }
   
-  checkS(event) {    
-    if(!event.match(/[^a-zA-Z]+/g)){
-      return event;
+  checkS(value):boolean{    
+    if(!value.match(/[^a-zA-Z]+/g)){
+      return true;
     }else{
-      return event.substring(0, event.length - 1); 
+      return false;
     }
   } 
   
   submitted = false;
-
   onSubmit() { this.submitted = true; }
   
-  click(){
+  checkRequiredValues():boolean {
+    var check:boolean = true;
+  
+    if(!this.checkValidation(this.model.firstName1, 'primer nombre') || !this.checkValidation(this.model.lastName1, 'primer apellido') ){
+      check = false;  
+    }else{
+      if(this.model.firstName2 != ''){
+        if(!this.checkS(this.model.firstName2)){
+          check = false;
+          this.errorMessage = 'El campo de segundo nombre solo puede contener letras (A-Z) y espacios';
+          this.error = true;
+        }
+      }
+      if(this.model.lastName2 != ''){
+        if(!this.checkS(this.model.lastName2)){
+          check = false;
+          this.errorMessage = 'El campo de segundo apellido solo puede contener letras (A-Z) y espacios';
+          this.error = true;
+        }
+      }
+    }
+  
+    return check;
+  }
+  
+  checkValidation(value, message):boolean {
+    var check:boolean = false;
+    if(value == ''){
+      this.errorMessage = 'El campo de ' + message + ' es obligatorio';
+      this.error = true;
+    }else if(!this.checkS(value)){
+      this.errorMessage = 'El campo de ' + message + ' solo puede contener letras (A-Z) y espacios';
+      this.error = true;
+    }else{
+      this.errorMessage = '';
+      this.error = false;
+      check = true;
+    }
+    return check;
+  }
+  
+  
+  finish():void{
     this.showBasicInfo = false;
     this.showFinancialInfo = true;
   }
