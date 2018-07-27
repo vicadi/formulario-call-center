77089c8ffe0dc8b88b71f49f6fefbe96b03e6c69
Xenia Viviana Cadena Diaz <xcadena@MacBook-Pro-de-adseglocdom.local>
2018-07-27 13:14:13 -0500

    delivery office

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,8 +13,6 @@ import { FormsModule } from '@angular/forms';
 import { NavBarUserComponent } from './nav-bar-user/nav-bar-user.component';
 import { HomeComponent } from './home/home.component';
 import { DeliveryCardComponent } from './delivery-card/delivery-card.component';
-import { DeliveryCardHomeComponent } from './delivery-card-home/delivery-card-home.component';
-import { DeliveryCardOfficeComponent } from './delivery-card-office/delivery-card-office.component';
 import { UserLoginComponent } from './user-login/user-login.component';
 
 @NgModule({
@@ -27,8 +25,6 @@ import { UserLoginComponent } from './user-login/user-login.component';
     HomeComponent,
     BasicinfoComponent,
     DeliveryCardComponent,
-    DeliveryCardHomeComponent,
-    DeliveryCardOfficeComponent,
     UserLoginComponent
   ],
   imports: [
--- src/app/delivery-card/delivery-card.component.ts
+++ src/app/delivery-card/delivery-card.component.ts
@@ -10,6 +10,18 @@ export class DeliveryCardComponent implements OnInit {
   showHome: boolean = true;
   showOffice: boolean = false;
 
+  city: string;
+  neighborhood: string;
+  address: string;
+
+  showBogota: boolean = false;
+  showCovenas: boolean = false;
+  showPlanetaRica: boolean = false;
+  names: string[] = ['Oficina Principal de Bogotá','Oficina Coveñas','Oficina Planeta Rica'];
+  addresses: string[] = ['Carrera 13 Nº 45 - 40','Carrera 16 N 12 -13','Calle 14 N 12 -14'];
+  nameOffice: string = '';
+  addressOffice: string = '';
+
   constructor() { }
 
   ngOnInit() {
@@ -18,11 +30,47 @@ export class DeliveryCardComponent implements OnInit {
   onClickHome(){
     this.showHome = true;
     this.showOffice = false;
+    this.city='';
+    this.neighborhood='';
+    this.address='';
   }
 
   onClickOffice(){
     this.showHome = false;
     this.showOffice = true;
+    this.showBogota = false;
+    this.showCovenas = false;
+    this.showPlanetaRica = false;
+    this.nameOffice = '';
+    this.addressOffice = '';
+  }
+
+  onClickBogota(){
+    this.showBogota = true;
+    this.showCovenas = false;
+    this.showPlanetaRica = false;
+    this.nameOffice = this.names[0];
+    this.addressOffice = this.addresses[0];
+  }
+
+  onClickCovenas(){
+    this.showBogota = false;
+    this.showCovenas = true;
+    this.showPlanetaRica = false;
+    this.nameOffice = this.names[1];
+    this.addressOffice = this.addresses[1];
+  }
+
+  onClickPlanetaRica(){
+    this.showBogota = false;
+    this.showCovenas = false;
+    this.showPlanetaRica = true;
+    this.nameOffice = this.names[2];
+    this.addressOffice = this.addresses[2];
+  }
+
+  showComponent(){
+    alert("Tu tarjeta de crédito esta en camino");
   }
 
 }
